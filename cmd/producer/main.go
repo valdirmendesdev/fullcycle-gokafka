95723eab3fdc991289293eaff6addf865435da3e
Valdir Mendes <valdir.mfjesus@hotmail.com>
2021-07-01 21:14:28 -0300

    Adding synchronous delivery channel

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -6,8 +6,19 @@ import(
 )
 
 func main() {
+	deliveryChan := make(chan kafka.Event)
 	producer := NewKafkaProducer()
-	Publish("mensagem", "teste", producer, nil)
+	Publish("mensagem", "teste", producer, nil, deliveryChan)
+
+	e := <-deliveryChan
+	msg := e.(*kafka.Message)
+
+	if msg.TopicPartition.Error != nil {
+		log.Fatal("Erro ao enviar")
+	} else {
+		log.Println("Mensagem enviada", msg.TopicPartition)
+	}
+
 	producer.Flush(1000)
 }
 
@@ -23,7 +34,7 @@ func NewKafkaProducer() *kafka.Producer {
 	return p
 }
 
-func Publish(msg string, topic string, producer *kafka.Producer, key []byte) error {
+func Publish(msg string, topic string, producer *kafka.Producer, key []byte, deliveryChan chan kafka.Event) error {
 	message := &kafka.Message{
 		TopicPartition: kafka.TopicPartition{
 			Topic:     &topic,
@@ -32,7 +43,7 @@ func Publish(msg string, topic string, producer *kafka.Producer, key []byte) err
 		Value:          []byte(msg),
 		Key:            key,
 	}
-	err := producer.Produce(message, nil)
+	err := producer.Produce(message, deliveryChan)
 	if err != nil {
 		return err
 	}
